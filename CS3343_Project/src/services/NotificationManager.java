f0b65a42a66cb2ab1d402d250cb2dc34be1fe19a
HarryLeeCity <kinflee9-c@my.cityu.edu.hk>
2025-10-23 11:49:47 +0800

    Update NotificationManager.java

--- CS3343_Project/src/services/NotificationManager.java
+++ CS3343_Project/src/services/NotificationManager.java
@@ -1,5 +1,6 @@
 package Services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import Objects.Notification;
@@ -7,11 +8,19 @@ import Objects.Notification;
 public class NotificationManager {
     private List<Notification> notifications;
 
+    public NotificationManager() {
+        this.notifications = new ArrayList<>();
+    }
+
     public boolean addNotifications(Notification notification) {
         return notifications.add(notification);
     }
 
     public boolean deleteNotifications(Notification notification) {
         return notifications.remove(notification);
+    }
+
+    public List<Notification> getNotifications() {
+        return notifications;
     }
 }
