399a6ee1abccfd513c8d6eca4130c9a6ed76838c
ALauuuuu <wllau76-c@my.cityu.edu.hk>
2025-11-16 17:42:22 +0800

    Create new testing file

---
+++ CS3343_Project/src/Test/ItemInventoryTest.java
@@ -0,0 +1,16 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import Instances.ItemInventory;
+
+public class ItemInventoryTest {
+    
+}
---
+++ CS3343_Project/src/Test/ItemTest.java
@@ -0,0 +1,16 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import Objects.Item;
+
+public class ItemTest {
+    
+}
