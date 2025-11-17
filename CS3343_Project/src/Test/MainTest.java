54b727f85857850a8cfb0771e58e8905c5a0ccf4
ALauuuuu <wllau76-c@my.cityu.edu.hk>
2025-11-18 00:42:34 +0800

    Update MainTest.java

---
+++ CS3343_Project/src/Test/MainTest.java
@@ -0,0 +1,19 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import Menu.Menu;
+import User.Admin;
+import User.Customer;
+import Main.Main;
+
+public class MainTest {
+
+}
